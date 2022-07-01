a435146a408baa1f6eb0fe503f0e2e1de4d2394d
patromalley <patromalley@gmail.com>
2022-07-01 13:31:22 -0500

    main

--- setEnvVars.sh
+++ setEnvVars.sh
@@ -1,3 +1,3 @@
 export GITHUB_USERNAME="patromalley"
-export GITHUB_REPO="bootcamp-2022-sls-api--omalley"
+export GITHUB_REPO="bootcamp-2022-sls-api--omalley-"
 export BUCKET_NAME="patromalley-bootcamp-2022-bucket" #username must be all lowercase
